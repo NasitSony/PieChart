bd988faca7fe1d7e8a4515b829ee23f3e449e5bc
NasitSony <nasitsony@imp-s050.impulsebd.local>
2013-09-11 12:54:06 +0600

    Third Commit

---
+++ PieChart/PieChart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PieChart
+//
+//  Created by Nasit Sarwar Sony on 9/11/13.
+//  Copyright (c) 2013 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PieAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PieAppDelegate class]));
+    }
+}
